5e3b3742290c3615be986b8c1ee4f67061260c43
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-11 19:35:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,37 +1,4 @@
-def new_hash
- new_hash = {}
-end
-
-def my_hash
-  grocery = {
-    :banana => 8
-  }
-end
-
-def pioneer
-  pioneer = {:name => "Grace Hopper"}
-end
-
-def id_generator
-  id_generator = {:id => 8}
-end
-
-def my_hash_creator(key, value)
-  my_hash_creator = {key => value }
-end
-
-def read_from_hash(hash, key)
-hash[key]
-end
-
-def update_counting_hash(hash, key)
-if hash[key]
-  hash[key]+=1
- else 
-hash[key] = 1
-end
-hash
-end
+Intro to Hashes
 
 
                            
